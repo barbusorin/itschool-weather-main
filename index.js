0f5d0a7298d877a409d353943382b8dee72e002e
barbusorin <barbu.sorin19@gmail.com>
2023-12-30 13:35:02 +0200

    refresh go to top button visibility fix

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const currentCityTag = document.querySelector(".current-city");
 let currentCity = localStorage.getItem("city");
 const scrollToTopButton = document.querySelector(".scroll-to-top");
 
-scrollToTopButton.addEventListener("click", () =>{
-  window.scrollTo(0, 0);
-})
 
-document.addEventListener("scroll", () =>{
+
+const refreshButtonVisibility = () => {
   let viewportHeight = window.innerHeight;
   if (document.documentElement.scrollTop < viewportHeight/2) {
     scrollToTopButton.style.display = "none";
   } else {
     scrollToTopButton.style.display = "block";
   }
+};
+refreshButtonVisibility();
+
+scrollToTopButton.addEventListener("click", () =>{
+  window.scrollTo(0, 0);
+})
+
+document.addEventListener("scroll", () =>{
+  refreshButtonVisibility()
 })
 
 // Daca nu avem oras salvat in localStorage, salvam orasul default, adica Bucuresti.
